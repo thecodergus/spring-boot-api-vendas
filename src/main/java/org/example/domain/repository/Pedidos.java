e01cc1cf420f8e9a34cc9b6c5f206f21266ff957
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-01-21 17:24:10 -0300

    Corrigindo erro

--- src/main/java/org/example/domain/repository/Pedidos.java
+++ src/main/java/org/example/domain/repository/Pedidos.java
@@ -3,11 +3,15 @@ package org.example.domain.repository;
 import org.example.domain.entity.Cliente;
 import org.example.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
     List<Pedido> findByCliente(Cliente cliente);
-    Optional<Pedido> findByIdFetchItens(Integer id);
+
+    @Query("select p from Pedido p left join fetch p.itens where p.id = :id")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
 }
